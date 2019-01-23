9c970cca5f11ad69005a09a6066f6a285f31770a
Andre <andre.luiz@gec.inatel.br>
2019-01-23 19:36:38 -0200

    Js file with navigations imports

---
+++ App.js
@@ -0,0 +1,17 @@
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import HomeScreen from './src/screens/HomeScreen'
+import TripsScreen from './src/screens/TripsScreen'
+import TripScreen from './src/screens/TripScreen'
+import AddTripScreen from './src/screens/AddTripScreen'
+import AddPointScreen from './src/screens/AddPointScreen'
+
+const AppNavigator = createStackNavigator({
+  Home: HomeScreen,
+  Trips: TripsScreen,
+  Trip: TripScreen,
+  AddTrip: AddTripScreen,
+  AddPoint: AddPointScreen
+  
+}, { initialRouteName: 'Home' })
+
+export default createAppContainer(AppNavigator)
